350edf197347bbca6f46b8932bdbfc225b8da201
Shyam <m.shyamsundar37@gmail.com>
2024-04-05 11:48:55 +0530

    latest

--- src/RoomScreen.tsx
+++ src/RoomScreen.tsx
@@ -1,15 +1,179 @@
-import styles from './RoomScreen.module.css'
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import { useState } from 'react';
-function RoomScreen(){
-    const [currentTab, setCurrentTab] = useState("create");
-    return <Tabs
-        value={currentTab}
-        onChange={(event: React.SyntheticEvent, newValue: string)=>setCurrentTab(newValue)}
-      >
-        <Tab value="create" label="Item Two" />
-        <Tab value="join" label="Item Three" />
-      </Tabs>
+import styles from "./RoomScreen.module.css";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { useEffect, useState } from "react";
+import TabPanel from "@mui/lab/TabPanel";
+import { TabContext, TabList } from "@mui/lab";
+import { Box, Button, CircularProgress, TextField } from "@mui/material";
+import { ref, set, push, onValue, get, child, update } from "firebase/database";
+import { database } from ".";
+import { useNavigate } from "react-router-dom";
+
+function RoomScreen() {
+  const [currentTab, setCurrentTab] = useState("create");
+  const [creatorName, setCreatorName] = useState("");
+  const [enteredRoomId, setEnteredRoomId] = useState<{
+    val: string;
+    isError: boolean;
+  }>({ val: "", isError: false });
+  const [joinerName, setJoinerName] = useState<{
+    val: string;
+    isError: boolean;
+  }>({ val: "", isError: false });
+  const [creatorOppName, setCreatorOppName] = useState<string>(null);
+  const [genRoomId, setGenRoomId] = useState<string>();
+  const [joinedStatus, setJoinedStatus] = useState(false);
+  const roomsRef = ref(database, "Rooms");
+  const navigate = useNavigate();
+  const handleTabChange = (event: React.SyntheticEvent, newValue: string) =>
+    setCurrentTab(newValue);
+
+  useEffect(() => {
+    if (genRoomId) {
+      const otherPlayerRef = ref(database, "Rooms/" + genRoomId + "/p2");
+      onValue(otherPlayerRef, (snap) => {
+        setCreatorOppName(snap.val());
+      });
+    }
+  }, [genRoomId]);
+  useEffect(() => {
+    if (!enteredRoomId.isError) {
+      const ref_ = child(roomsRef, "-" + enteredRoomId.val + "/start");
+      onValue(ref_, (snap) => {
+        console.log(snap.val());
+        if (snap.val() !== null) {
+          navigate("/GamePage?mode=online", {
+            state: { roomId: "-" + enteredRoomId.val, isHost: false },
+          });
+        }
+      });
+    }
+  }, [enteredRoomId]);
+  async function generateIdClick() {
+    await set(roomsRef, null);
+    const Id = await push(roomsRef, {
+      p1: creatorName,
+      data: ["", "", "", "", "", "", "", "", ""],
+    }).key;
+    setGenRoomId(Id);
+  }
+  async function StartGameClickHandler() {
+    await update(child(roomsRef, genRoomId), { start: "yes" });
+    navigate("/GamePage?mode=online", {
+      state: { roomId: genRoomId, isHost: true },
+    });
+  }
+  async function JoinClickHandler() {
+    const reference = child(roomsRef, "-" + enteredRoomId.val + "/p1");
+    const resp = await get(reference);
+    if (resp.val() === null) {
+      setEnteredRoomId((prev) => ({ ...prev, isError: true }));
+    } else {
+      const ref_ = child(roomsRef, "-" + enteredRoomId.val);
+      await update(ref_, { p2: joinerName.val });
+      setJoinedStatus(true);
+    }
+  }
+  function ExitClickHandler() {
+    setJoinedStatus(false);
+  }
+  return (
+    <>
+      <TabContext value={currentTab}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList onChange={handleTabChange} aria-label="lab API tabs example">
+            <Tab label="Create Room" value="create" />
+            <Tab label="Join Room" value="join" />
+          </TabList>
+        </Box>
+        <TabPanel value="create" className={styles.CreateRoot}>
+          <TextField
+            id="outlined-basic"
+            label="Enter your name"
+            variant="outlined"
+            value={creatorName}
+            onChange={(event) => setCreatorName(event.target.value)}
+          />
+          <Button variant="outlined" onClick={generateIdClick}>
+            Generate Room Id
+          </Button>
+          {genRoomId && (
+            <div>
+              Generated room ID :{" "}
+              <span style={{ color: "#56a326" }}>{genRoomId}</span> Please share
+              this code to invite them
+            </div>
+          )}
+          {genRoomId && creatorOppName === null && (
+            <div>
+              <CircularProgress size={20} />
+              <span> Waiting for friend to join.....</span>
+            </div>
+          )}
+          {creatorOppName && (
+            <div>
+              <div>Joined Player Name : {creatorOppName}</div>
+            </div>
+          )}
+          {creatorOppName && (
+            <Button
+              color="success"
+              variant="contained"
+              onClick={StartGameClickHandler}
+            >
+              Start Game
+            </Button>
+          )}
+        </TabPanel>
+        <TabPanel value="join" className={styles.JoinRoot}>
+          {!joinedStatus && (
+            <TextField
+              error={joinerName.isError}
+              id="outlined-basic"
+              label="Enter name"
+              variant="outlined"
+              value={joinerName.val}
+              onChange={(event) =>
+                setJoinerName((prev) => ({
+                  ...prev,
+                  val: event.target.value,
+                  isError: false,
+                }))
+              }
+            />
+          )}
+          {!joinedStatus && (
+            <div className={styles.joinInput}>
+              <TextField
+                error={enteredRoomId.isError}
+                id="outlined-basic"
+                label="Enter Room Id"
+                variant="outlined"
+                value={enteredRoomId.val}
+                onChange={(event) =>
+                  setEnteredRoomId((prev) => ({
+                    ...prev,
+                    val: event.target.value,
+                    isError: false,
+                  }))
+                }
+              />
+              <Button variant="contained" onClick={JoinClickHandler}>
+                Join
+              </Button>
+            </div>
+          )}
+          {joinedStatus && (
+            <div>Joined Room Successfully, ask host to start the game</div>
+          )}
+          {joinedStatus && (
+            <Button color="error" variant="outlined" onClick={ExitClickHandler}>
+              Exit ?
+            </Button>
+          )}
+        </TabPanel>
+      </TabContext>
+    </>
+  );
 }
-export default RoomScreen
+export default RoomScreen;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 import React from "react";
+import { initializeApp } from "firebase/app";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/Store";
+import { getDatabase } from "firebase/database";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyApbM6Z33UhsNHL0abrjvchfdXipBRQD6A",
+  authDomain: "tic-tac-toe-22235.firebaseapp.com",
+  projectId: "tic-tac-toe-22235",
+  storageBucket: "tic-tac-toe-22235.appspot.com",
+  messagingSenderId: "796347971459",
+  appId: "1:796347971459:web:467df711d720f2957428b6",
+  measurementId: "G-SQZY38Y5SD",
+  databaseURL: "https://tic-tac-toe-22235-default-rtdb.firebaseio.com/",
+};
+export const firebaseApp = initializeApp(firebaseConfig);
+export const database = getDatabase(firebaseApp);
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
